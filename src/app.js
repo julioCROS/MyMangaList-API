c2f1d888203956dadb2007a5a857f0c0a880944c
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-04-15 17:55:47 -0300

    [+] add CROS

--- src/app.js
+++ src/app.js
@@ -19,9 +19,23 @@ const indexRoute = require('./routes/indexRouter');
 const mangaRoute = require('./routes/mangaRouter');
 const userRoute = require('./routes/userRouter');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({
+  limit: '10mb'
+}));
+app.use(bodyParser.urlencoded({ 
+  extended: false
+}));
 
+// Habilita o CROS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+
+// Rotas
 app.use('/', indexRoute);
 app.use('/mangas', mangaRoute);
 app.use('/users', userRoute);
