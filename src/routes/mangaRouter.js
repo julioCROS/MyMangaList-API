dd170ee526971d26d3a8028870dbbeb42d252dec
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-04-15 10:37:04 -0300

    authService on router

--- src/routes/mangaRouter.js
+++ src/routes/mangaRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/mangaController');
+const authService = require('../services/authService');
 
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
 router.get('/id/:id', controller.getById);
 router.get('/genres/:genres', controller.getByGenres);
-router.post('/', controller.post);
-router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.post('/', authService.authorize, controller.post);
+router.put('/:id', authService.authorize, controller.put);
+router.delete('/:id', authService.authorize, controller.delete);
 
 module.exports = router;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
+const authService = require('../services/authService');
 
-router.get('/', controller.get);
-router.get('/id/:id', controller.getById);
+router.get('/', authService.authorize, controller.get);
+router.get('/id/:id', authService.authorize, controller.getById);
 router.post('/', controller.post);
-router.put('/:id', controller.put);
-router.delete('/:id', controller.delete);
+router.put('/:id', authService.authorize, controller.put);
+router.delete('/:id', authService.authorize, controller.delete);
 
 module.exports = router;
